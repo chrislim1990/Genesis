4beb65a220dec47e7e1befdafab62ce85d7de337
chrislim1990 <tlyon_90@hotmail.com>
2015-06-20 15:54:08 +0800

    updated bower workflow

--- grunt/contrib-copy.js
+++ grunt/contrib-copy.js
@@ -3,20 +3,32 @@ module.exports = function(grunt) {
 	grunt.config('copy', {
 
 		resaveScss: {
-			files: [
-			{
-				expand: true,
-				flatten: true,
-				cwd: 'bower_components/',
-				src: ['normalize.css/normalize.css'], 
-				dest: 'src/sass/vendors/',
-				rename: function(dest, src) {
-					return dest + src.replace(/\.css$/, ".scss");
-				}
+			expand: true,
+			flatten: true,
+			cwd: 'bower_components/',
+			src: ['normalize.css/normalize.css'], 
+			dest: 'src/sass/vendors/',
+			rename: function(dest, src) {
+				return dest + src.replace(/\.css$/, ".scss");
 			}
-			],
 		},
 
+		dist: {
+			files: [
+			{ expand: true, flatten: true, src: ['src/*'], dest: 'dist/', filter: 'isFile', },
+			{ expand: true, cwd: 'src/', src: ['vendors/*'], dest: 'dist/', }
+			]
+		},
+
+		updatePath: {
+			src: 'dist/utilities.scss',
+			dest: 'dist/utilities.scss',
+			options: {
+				process: function (content, srcpath) {
+					return content.replace('bower_components',"..");
+				},
+			},
+		}
 	});
 
 	grunt.loadNpmTasks('grunt-contrib-copy');
